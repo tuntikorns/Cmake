49000f0228e56502e35401fcfd0c1b0800b18ab2
tunti <tunti@zensorium.com>
2022-08-11 14:13:53 +0700

    Update Step8

--- MathFunctions/mysqrt.cxx
+++ MathFunctions/mysqrt.cxx
@@ -1,22 +1,24 @@
 #include <iostream>
 #include "MathFunctions.h"
-#include <cmath>
+
+// include the generated table
 #include "Table.h"
 
+namespace mathfunctions {
+namespace detail {
 // a hack square root calculation using simple operations
 double mysqrt(double x)
 {
   if (x <= 0) {
     return 0;
   }
-  
- // use the table to help find an initial value
+
+  // use the table to help find an initial value
   double result = x;
   if (x >= 1 && x < 10) {
     std::cout << "Use the table to help find an initial value " << std::endl;
     result = sqrtTable[static_cast<int>(x)];
   }
-
 
   // do ten iterations
   for (int i = 0; i < 10; ++i) {
@@ -29,4 +31,6 @@ double mysqrt(double x)
   }
 
   return result;
+}
+}
 }
